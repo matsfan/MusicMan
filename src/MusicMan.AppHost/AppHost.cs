ee7dbec3d16d260e8df83b7d46f4ddaaf9910ec2
Mike Price <idflyfish@hotmail.com>
2025-09-15 19:42:26 -0600

    renamed the WebApi project

--- src/MusicMan.AppHost/AppHost.cs
+++ src/MusicMan.AppHost/AppHost.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.MusicMan_Web>("musicman-web");
+builder.AddProject<Projects.MusicMan_WebApi>("musicman-web");
 
 builder.Build().Run();
